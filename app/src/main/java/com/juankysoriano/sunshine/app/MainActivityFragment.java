13de0137cc9ae8a90e8ad4ff77cf3236e3199206
Juanky Soriano <juanky@juankysoriano.com>
2015-11-01 20:28:40 +0000

    Introduce listview with fake data

--- app/src/main/java/com/juankysoriano/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/juankysoriano/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,25 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
+
+        List<String> weekForecast = new ArrayList<>(5);
+        weekForecast.add("Sunday cold");
+        weekForecast.add("Saturday cold");
+        weekForecast.add("Monday cold");
+        weekForecast.add("Friday cold");
+        weekForecast.add("Tuesday cold");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast
+        );
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
 }
